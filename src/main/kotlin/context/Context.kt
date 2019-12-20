fe44cdd6cfa1a8da431f39a923a9d9d1178313e4
Francisco Ruiz <franrv@gmail.com>
2019-12-20 18:06:28 +0000

    changed spark for ktor

--- src/main/kotlin/context/Context.kt
+++ src/main/kotlin/context/Context.kt
@@ -2,9 +2,17 @@ package context
 
 import com.mongodb.client.MongoDatabase
 import controller.ItemListController
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 import model.Item
-import org.litote.kmongo.*
-import spark.kotlin.*
+import org.litote.kmongo.KMongo
+import org.litote.kmongo.getCollection
+
 
 class Context {
 
@@ -16,8 +24,15 @@ class Context {
         fun main(args: Array<String>) {
             prepareDummyData()
             val itemListController = ItemListController()
-            val http: Http = ignite()
-            http.get("/retrieveItems") { itemListController.retrieveItemsList() }
+
+            embeddedServer(Netty, 8080) {
+                routing {
+                    get("/") {
+
+                        call.respondText(itemListController.retrieveItemsList(), ContentType.Application.Json)
+                    }
+                }
+            }.start(wait = true)
         }
 
         private fun prepareDummyData() {
